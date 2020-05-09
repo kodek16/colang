abc85fa21eacf86c1b5cda7e53a90fd01cfdbccf
Pavel Senchanka <pavel.senchanka@gmail.com>
2020-05-09 11:45:01 +0200

    Document the backend interface.

--- colang/src/backends/mod.rs
+++ colang/src/backends/mod.rs
@@ -1,13 +1,17 @@
-//! This module contains various backends for CO compiler.
-//! Backend is the part of the compiler where the code is sent to
-//! after it's compiled and type-checked.
+//! CO compiler backend interface definition.
+//!
+//! Backend is a part of the compiler that receives the compiled and type-checked program IR and
+//! does something useful with it, for example, translating it into a target language, or executing
+//! it directly.
 
 use crate::program::Program;
 
-/// Common backend interface.
+/// Common interface for CO compiler backends.
 pub trait Backend {
-    /// Process the compiled CO program. Backends should report any user-caused errors to
-    /// the user, and panic if any internal error occurs. The returned `Result` indicates
-    /// whether any user-caused errors occurred and were reported.
+    /// Process the compiled CO program.
+    ///
+    /// Backends should report any user-caused errors to the user, and panic only if an internal
+    /// error occurs. The returned `Result` indicates whether any user-caused errors occurred and
+    /// were reported.
     fn run(&self, file_name: &str, source: &str, program: Program) -> Result<(), ()>;
 }

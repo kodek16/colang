45af1f8ef60de28c84fc63bfb770fe6ba7160f46
Pavel Senchanka <pavel.senchanka@gmail.com>
2020-09-27 14:46:58 +0100

    Extend experimental parser so it's able to parse an empty function.

--- colang/src/parser/mod.rs
+++ colang/src/parser/mod.rs
@@ -22,37 +22,92 @@ pub fn parse(source_code: &str, file: InputSpanFile) -> Result<ast::Program, Vec
     let ParseResult(program, _) = program(input, &ctx);
     match program {
         ParsedNode::Ok(program) => Ok(program),
-        ParsedNode::BestGuess(_, errors) => Err(errors),
+        ParsedNode::Recovered(_, errors) => Err(errors),
+        ParsedNode::Missing(error) => Err(vec![error]),
     }
 }
 
 /// Node parsed by a parsing routine.
 ///
-/// `Ok` case is the actual node produced by a successful parse. The other case
-/// is a best-effort guess (or flat out stub) of what a successful parse could be.
+/// `Ok` case is the actual node produced by a successful parse. `Recovered` is produced
+/// when the parser is sure that some part of the expected node was present and consumed,
+/// but the parse was not fully successful. `Missing` appears when the expected node was not
+/// found, and nothing was consumed.
 enum ParsedNode<T> {
     Ok(T),
-    BestGuess(T, Vec<SyntaxError>),
+    Recovered(T, Vec<SyntaxError>),
+    Missing(SyntaxError),
 }
 
 /// Result of running a parsing routine.
 ///
-/// First element is the parsed node, second is the remainder of the input
-/// that was not touched by the parse.
+/// First element is the parsed node, second is the remainder of the input that was not consumed
+/// by the parse.
 struct ParseResult<'a, T>(ParsedNode<T>, Input<'a>);
 
-// Attempts to parse a sub-node and terminates immediately on failure.
-// All consumed input (including this call) is still considered consumed.
-// Guess is made based on the argument only, not using the sub-node guess.
-macro_rules! try_parse {
-    ($e: expr, $guess: expr) => {{
+/// Attempts to parse a node and exits immediately with `Missing` on failure.
+///
+/// This should be used for "anchor" nodes (e.g. keywords in the beginning of a production).
+macro_rules! try_parse_or_abort {
+    ($e: expr, $errors: expr) => {{
         let ParseResult(node, remaining) = $e;
         match node {
             ParsedNode::Ok(node) => (node, remaining),
-            ParsedNode::BestGuess(_, errors) => {
-                return ParseResult(ParsedNode::BestGuess($guess, errors), remaining);
+            ParsedNode::Recovered(node, mut errors) => {
+                $errors.append(&mut errors);
+                (node, remaining)
+            }
+            ParsedNode::Missing(error) => {
+                return ParseResult(ParsedNode::Missing(error), remaining);
             }
         }
+    }};
+}
+
+/// Attempts to parse a node, falling back to a stub if not successful.
+///
+/// This effectively performs "recovery by insertion".
+macro_rules! try_parse_or_synthesise {
+    ($e: expr, $fallback: expr, $errors: expr) => {{
+        let ParseResult(node, remaining) = $e;
+        match node {
+            ParsedNode::Ok(node) => (node, remaining),
+            ParsedNode::Recovered(node, mut errors) => {
+                $errors.append(&mut errors);
+                (node, remaining)
+            }
+            ParsedNode::Missing(error) => {
+                $errors.push(error);
+                ($fallback, remaining)
+            }
+        }
+    }};
+}
+
+/// Attempts to parse a node that is optional in the current position.
+macro_rules! try_parse_opt {
+    ($e: expr, $errors: expr) => {{
+        let ParseResult(node, remaining) = $e;
+        match node {
+            ParsedNode::Ok(node) => (Some(node), remaining),
+            ParsedNode::Recovered(node, mut errors) => {
+                $errors.append(&mut errors);
+                (Some(node), remaining)
+            }
+            ParsedNode::Missing(_) => (None, remaining),
+        }
+    }};
+}
+
+/// Completes a parse choosing an appropriate `ParsedNode` variant.
+macro_rules! complete_parse {
+    ($node: expr, $remaining: expr, $errors: expr) => {{
+        let node = if ($errors.is_empty()) {
+            ParsedNode::Ok($node)
+        } else {
+            ParsedNode::Recovered($node, $errors)
+        };
+        ParseResult(node, $remaining)
     }};
 }
 
@@ -74,8 +129,14 @@ fn program<'a>(mut input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, as
 
         match function_def {
             ParsedNode::Ok(function_def) => functions.push(function_def),
-            ParsedNode::BestGuess(_, mut es) => {
+            ParsedNode::Recovered(function_def, mut es) => {
+                functions.push(function_def);
                 errors.append(&mut es);
+                input = recover_to_next_global(input);
+            }
+            ParsedNode::Missing(e) => {
+                errors.push(e);
+                // TODO: this is probably useless as we are supposed to be in a good location?
                 input = recover_to_next_global(input);
             }
         }
@@ -86,14 +147,7 @@ fn program<'a>(mut input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, as
         structs: vec![],
         traits: vec![],
     };
-
-    let node = if errors.is_empty() {
-        ParsedNode::Ok(program)
-    } else {
-        ParsedNode::BestGuess(program, errors)
-    };
-
-    ParseResult(node, input)
+    complete_parse!(program, input, errors)
 }
 
 fn recover_to_next_global(input: Input) -> Input {
@@ -105,31 +159,56 @@ fn recover_to_next_global(input: Input) -> Input {
 }
 
 fn function_def<'a>(input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, ast::FunctionDef> {
-    let guess = ast::FunctionDef {
-        name: ast::Identifier {
-            text: "<error>".to_string(),
-            span: input.span_of_first(ctx),
+    let mut errors = vec![];
+
+    let (fun_span, input) = try_parse_or_abort!(kw_fun(input, ctx), errors);
+    let (name, input) = try_parse_or_synthesise!(
+        ident(input, ctx),
+        ast::Identifier {
+            text: "<missing>".to_string(),
+            span: input.span_of_first(ctx)
         },
-        parameters: vec![],
-        return_type: None,
-        body: None,
-        signature_span: input.span_of_first(ctx),
-    };
+        errors
+    );
+    let (_, input) = try_parse_or_synthesise!(
+        chars_exact(input, ctx, "("),
+        input.span_of_first(ctx),
+        errors
+    );
+    let (_, input) = try_parse_or_synthesise!(
+        chars_exact(input, ctx, ")"),
+        input.span_of_first(ctx),
+        errors
+    );
+    let (body, input) = try_parse_opt!(block(input, ctx), errors);
 
-    let signature_begin_span = input.span_of_first(ctx);
-    let ((), input) = try_parse!(kw_fun(input, ctx), guess);
-    let (name, input) = try_parse!(ident(input, ctx), guess);
-
-    let signature_span = signature_begin_span + name.span;
+    let signature_span = fun_span + name.span;
     let function_def = ast::FunctionDef {
         name,
         parameters: vec![],
         return_type: None,
-        body: None,
+        body: body.map(ast::Expression::Block),
         signature_span,
     };
+    complete_parse!(function_def, input, errors)
+}
 
-    ParseResult(ParsedNode::Ok(function_def), input)
+fn block<'a>(input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, ast::BlockExpr> {
+    let mut errors = vec![];
+
+    let (left_brace_span, input) = try_parse_or_abort!(chars_exact(input, ctx, "{"), errors);
+    let (right_brace_span, input) = try_parse_or_synthesise!(
+        chars_exact(input, ctx, "}"),
+        input.span_of_first(ctx),
+        errors
+    );
+
+    let block = ast::BlockExpr {
+        span: left_brace_span + right_brace_span,
+        statements: vec![],
+        final_expr: None,
+    };
+    complete_parse!(block, input, errors)
 }
 
 fn ident<'a>(input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, ast::Identifier> {
@@ -145,16 +224,12 @@ fn ident<'a>(input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, ast::Ide
         }
         None => {
             let error = SyntaxError::UnexpectedToken(input.span_of_first(ctx));
-            let guess = ast::Identifier {
-                text: String::from("<error>"),
-                span: input.span_of_first(ctx),
-            };
-            ParseResult(ParsedNode::BestGuess(guess, vec![error]), input)
+            ParseResult(ParsedNode::Missing(error), input)
         }
     }
 }
 
-fn kw_fun<'a>(input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, ()> {
+fn kw_fun<'a>(input: Input<'a>, ctx: &ParsingContext) -> ParseResult<'a, InputSpan> {
     word_exact(input, ctx, "fun")
 }
 
@@ -162,7 +237,7 @@ fn word_exact<'a>(
     input: Input<'a>,
     ctx: &ParsingContext,
     target_word: &str,
-) -> ParseResult<'a, ()> {
+) -> ParseResult<'a, InputSpan> {
     let input = skip_ignored(input);
 
     let idx = input.find(|c: char| !c.is_alphanumeric());
@@ -172,11 +247,31 @@ fn word_exact<'a>(
     };
 
     if *actual_word == *target_word {
-        ParseResult(ParsedNode::Ok(()), remaining)
+        ParseResult(ParsedNode::Ok(actual_word.span_of_full(ctx)), remaining)
     } else {
         // TODO maybe see if the word is similar, and if it is, eat it?
         let error = SyntaxError::UnexpectedToken(actual_word.span_of_first(ctx));
-        ParseResult(ParsedNode::BestGuess((), vec![error]), input)
+        ParseResult(ParsedNode::Missing(error), input)
+    }
+}
+
+/// Consumes `target_chars`, expecting them to be the next non-whitespace characters on input.
+///
+/// Unlike `word_exact`, the characters do not have to be alphanumeric, and do not have to
+/// be followed by a specific boundary.
+fn chars_exact<'a>(
+    input: Input<'a>,
+    ctx: &ParsingContext,
+    target_chars: &str,
+) -> ParseResult<'a, InputSpan> {
+    let input = skip_ignored(input);
+
+    if &input[..target_chars.len()] == target_chars {
+        let (chars, remaining) = input.split_at(target_chars.len());
+        ParseResult(ParsedNode::Ok(chars.span_of_full(ctx)), remaining)
+    } else {
+        let error = SyntaxError::UnexpectedToken(input.span_of_first(ctx));
+        ParseResult(ParsedNode::Missing(error), input)
     }
 }
 
